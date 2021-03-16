c31d599634256d8206051423cf08a28858c5bf34
Thomas W. Kirk III <Thomas.Kirk@osii.com>
2021-03-15 19:33:30 -0500

    Create simple table Create simple get request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'restaurant-project';
+    title = 'restaurant-project';
+
+    /*URL for coding challenge*/
+    URL = 'https://code-challenge.spectrumtoolbox.com/api/restaurants';
+
+    restaurantData = <any>[];
+    headerDict = new HttpHeaders({Authorization: "Api-Key q3MNxtfep8Gt"});
+
+    constructor(
+        private http: HttpClient,
+    ) { }
+
+
+    ngOnInit() {
+        this.http.get<any[]>(this.URL, {headers: this.headerDict}).subscribe((data: any[]) => {
+            this.restaurantData = data;
+        });
+        console.log(this.restaurantData);
+    }
 }
